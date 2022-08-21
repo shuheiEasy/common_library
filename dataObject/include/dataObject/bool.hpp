b83f670397f8add73fad51ebf1f81d1d368a8a4d
easy_robot <wanzushuuhei@gmail.com>
2022-08-21 16:31:38 +0900

    コンストラクタ・変換設定

--- dataObject/include/dataObject/bool.hpp
+++ dataObject/include/dataObject/bool.hpp
@@ -2,22 +2,36 @@
 #define BOOL_H
 
 #include "./core.hpp"
+#include "./string.hpp"
 
 namespace dataObject
 {
-    class Bool:public DataObject{
-        private:
+    class Bool : public DataObject
+    {
+    private:
         bool _data;
 
         void _init();
+        void _fromInt(const int &data);
+        void _fromString(const String &data);
 
-        public:
+    public:
         Bool();
         Bool(const bool &data);
+        Bool(const Bool &data);
+        Bool(const int &data);
+        Bool(const String &data);
         ~Bool();
+        bool getBool() const;
         const char *getType() const;
         const char *getLog() const;
         int getSize() const;
+        operator bool() { return _data; };
+        Bool operator=(const bool &data);
+        Bool operator=(const Bool &data);
+        Bool operator=(const int &data);
+        Bool operator=(const String &data);
+
     };
 }
 
--- dataObject/src/bool.cpp
+++ dataObject/src/bool.cpp
@@ -6,13 +6,37 @@ Bool::Bool()
 {
     this->_init();
 }
+
 Bool::Bool(const bool &data)
 {
     this->_init();
     this->_data = data;
 }
 
+Bool::Bool(const Bool &data)
+{
+    this->_init();
+    this->_data = data.getBool();
+}
+
+Bool::Bool(const int &data)
+{
+    this->_init();
+    _fromInt(data);
+}
+
+Bool::Bool(const String &data)
+{
+    this->_init();
+    _fromString(data);
+}
+
 Bool::~Bool() {}
+
+bool Bool::getBool() const
+{
+    return this->_data;
+}
 
 const char *Bool::getType() const
 {
@@ -20,15 +44,70 @@ const char *Bool::getType() const
 }
 const char *Bool::getLog() const
 {
-    if(this->_data){
+    if (this->_data)
+    {
         return "True";
-    }else{
+    }
+    else
+    {
         return "False";
     }
 }
 int Bool::getSize() const
 {
     return 1;
+}
+
+Bool Bool::operator=(const bool &data)
+{
+    this->_data = data;
+    return *this;
+}
+
+Bool Bool::operator=(const Bool &data)
+{
+    this->_data = data.getBool();
+    return *this;
+}
+
+Bool Bool::operator=(const int &data)
+{
+    this->_fromInt(data);
+    return *this;
+}
+
+Bool Bool::operator=(const String &data)
+{
+    this->_fromString(data);
+    return *this;
+}
+
+void Bool::_fromInt(const int &data)
+{
+    if (data > 0)
+    {
+        this->_data = true;
+    }
+    else
+    {
+        this->_data = false;
+    }
+}
+
+void Bool::_fromString(const String &data)
+{
+    if (data == "True")
+    {
+        this->_data = true;
+    }
+    else if (data == "true")
+    {
+        this->_data = true;
+    }
+    else
+    {
+        this->_data = false;
+    }
 }
 
 void Bool::_init()
