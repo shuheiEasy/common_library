538efad3dfc6de0dee71b87a896458df79a147ba
easy_robot <wanzushuuhei@gmail.com>
2022-08-23 19:40:08 +0900

    operatorの種類追加

--- dataObject/src/int.cpp
+++ dataObject/src/int.cpp
@@ -72,6 +72,156 @@ bool Int::operator!=(const Int &data) const
 {
     return !operator==(data);
 }
+bool Int::operator<(const int &data) const
+{
+    if (this->_data < data)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Int::operator<(const Int &data) const
+{
+    if (this->_data < data.getInt())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Int::operator<=(const int &data) const
+{
+    if (this->_data <= data)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Int::operator<=(const Int &data) const
+{
+    if (this->_data <= data.getInt())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Int::operator>(const int &data) const
+{
+    if (this->_data > data)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Int::operator>(const Int &data) const
+{
+    if (this->_data > data.getInt())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Int::operator>=(const int &data) const
+{
+    if (this->_data >= data)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+bool Int::operator>=(const Int &data) const
+{
+    if (this->_data >= data.getInt())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+Int &Int::operator+=(const int &data)
+{
+    this->_data = this->_data + data;
+    return *this;
+}
+Int &Int::operator+=(const Int &data)
+{
+    this->_data = this->_data + data.getInt();
+    return *this;
+}
+Int &Int::operator-=(const int &data)
+{
+    this->_data = this->_data - data;
+    return *this;
+}
+Int &Int::operator-=(const Int &data)
+{
+    this->_data = this->_data - data.getInt();
+    return *this;
+}
+Int &Int::operator*=(const int &data)
+{
+    this->_data = this->_data * data;
+    return *this;
+}
+Int &Int::operator*=(const Int &data)
+{
+    this->_data = this->_data * data.getInt();
+    return *this;
+}
+Int &Int::operator/=(const int &data)
+{
+    this->_data = (int)(this->_data / data);
+    return *this;
+}
+Int &Int::operator/=(const Int &data)
+{
+    this->_data = (int)(this->_data / data.getInt());
+    return *this;
+}
+Int Int::operator++()
+{
+    *this += 1;
+    return *this;
+}
+Int Int::operator++(int)
+{
+    Int ret = *this;
+    *this += 1;
+    return ret;
+}
+Int Int::operator--()
+{
+    *this -= 1;
+    return *this;
+}
+Int Int::operator--(int)
+{
+    Int ret = *this;
+    *this -= 1;
+    return ret;
+}
 void Int::_init()
 {
     this->_data = 0;
