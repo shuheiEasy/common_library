a575e33e52c1c21451a1af8ed6a47fba720e13b4
iijimaRobot <wanzushuuhei@gmail.com>
2022-10-05 08:16:02 +0900

    雛形作成

---
+++ dataObject/include/dataObject/any.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file dataObject.hpp
+ * @author iijima-robot
+ * @brief Anyクラスを宣言するヘッダファイル
+ * @date 2022-09-24
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#ifndef COMMON_DATAOBJECT_ANY_H
+#define COMMON_DATAOBJECT_ANY_H
+
+#include <dataObject/core.hpp>
+
+namespace dataObject
+{
+    class Any
+    {
+    };
+}
+
+#endif
