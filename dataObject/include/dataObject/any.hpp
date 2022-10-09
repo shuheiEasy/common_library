6be1b82073714f96a39c18f9091d945f74146c09
iijimaRobot <wanzushuuhei@gmail.com>
2022-10-10 00:25:01 +0900

    ひな形作成

--- dataObject/include/dataObject/any.hpp
+++ dataObject/include/dataObject/any.hpp
@@ -10,12 +10,41 @@
 #ifndef COMMON_DATAOBJECT_ANY_H
 #define COMMON_DATAOBJECT_ANY_H
 
-#include <dataObject/core.hpp>
+#include <dataObject/dataObject.hpp>
 
 namespace dataObject
 {
-    class Any
+    /// @brief 任意の型を取れるクラス
+    /// @details dataObject型を入力する。
+    class Any : public DataObject
     {
+    private:
+        DataObject *_data;
+        String _type;
+
+    public:
+        Any();
+        template <class T>
+        Any(const T &data, typename std::enable_if<std::is_base_of<DataObject, T>::value>::type * = nullptr)
+        {
+            _data = new T(data);
+            _type = _data->getType();
+        }
+        ~Any()
+        {
+            if (_type != "")
+            {
+                delete _data;
+            }
+        }
+        template <class T>
+        T *getData()
+        {
+            return (T *)_data;
+        }
+        const char *getLog() const { return _data->getLog(); }
+        int getSize() const { return _data->getSize(); }
+         const char *getType() const { return _type.getChar(); }
     };
 }
 
