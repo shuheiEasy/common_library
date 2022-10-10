dbce0265c41919c1fa7a94ea24680479ba76ec1c
iijimaRobot <wanzushuuhei@gmail.com>
2022-10-10 12:58:17 +0900

    Any型の初版完成

--- dataObject/include/dataObject/any.hpp
+++ dataObject/include/dataObject/any.hpp
@@ -19,32 +19,78 @@ namespace dataObject
     class Any : public DataObject
     {
     private:
-        DataObject *_data;
-        String _type;
+        struct Storage
+        {
+            virtual ~Storage() {}
+            virtual Storage *copy() const { return nullptr; }
+            virtual const char *getLog() const { return ""; }
+            virtual int getSize() const { return 0; }
+            virtual const char *getType() const {return "Null";}
+        };
 
-    public:
-        Any();
-        template <class T>
-        Any(const T &data, typename std::enable_if<std::is_base_of<DataObject, T>::value>::type * = nullptr)
+        template <class T, typename std::enable_if<std::is_base_of<DataObject, T>::value>::type * = nullptr>
+        struct Data : public Storage
         {
-            _data = new T(data);
-            _type = _data->getType();
-        }
-        ~Any()
-        {
-            if (_type != "")
+            T *_data;
+            Data(const T &data)
+            {
+                _data = new T(data);
+            }
+            ~Data()
             {
                 delete _data;
             }
+            Storage *copy() const
+            {
+                return new Data<T>(*_data);
+            }
+            const char *getType() const { return _data->getType(); }
+            int getSize() const { return _data->getSize(); }
+            const char *getLog() const { return _data->getLog(); }
+        };
+
+        Storage *_data_ptr;
+
+    public:
+        /// @brief コンストラクタ
+        Any();
+        /// @brief コンストラクタ
+        /// @tparam T dataObject型の派生型
+        /// @param data 代入するデータ
+        /// @todo 将来的に算術型にも対応する予定
+        template <class T>
+        Any(const T &data, typename std::enable_if<std::is_base_of<DataObject, T>::value>::type * = nullptr)
+        {
+            _data_ptr = new Data<T>(data);
         }
+        /// @brief コピーコンストラクタ
+        Any(const Any &data){
+            _data_ptr = data._data_ptr->copy();
+        }
+        /// @brief デコンストラクタ
+        ~Any()
+        {
+            delete _data_ptr;
+        }
+        /// @brief 値を取得する関数
+        /// @tparam T 取得するデータの型
+        /// @return 取得する値
         template <class T>
         T *getData()
         {
-            return (T *)_data;
+            Data<T> *ptr = dynamic_cast<Data<T> *>(_data_ptr);
+            return ptr->_data;
         }
-        const char *getLog() const { return _data->getLog(); }
-        int getSize() const { return _data->getSize(); }
-         const char *getType() const { return _type.getChar(); }
+        const char *getLog() const { return _data_ptr->getLog(); }
+        int getSize() const { return _data_ptr->getSize(); }
+        const char *getType() const { return _data_ptr->getType(); }
+
+        template <class T, typename std::enable_if<std::is_base_of<DataObject, T>::value>::type * = nullptr>
+        Any operator=(const T &data){
+            delete _data_ptr;
+            _data_ptr = new Data<T>(data);
+            return *this;
+        }
     };
 }
 
